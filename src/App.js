6bed4d485282571f2671e164eb75f4aa3b9b6e10
soni895 <darshansoni895@gmail.com>
2023-08-01 11:02:04 +0530

    initail commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+
+
+
+import "./App.css"
+
+export default function App() {
+  
+
+  return (
+    <div className=" w-screen  h-screen flex items-center justify-center  bg-blue-500 text-white">
+      <h1>hi darshan soni</h1>
+    </div>
+  );
+
+}
